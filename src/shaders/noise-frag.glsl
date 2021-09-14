cb2a80fe8a95c2cd6eacd06885471da9998ffe9b
Amelia Peng <pqy@seas.upenn.edu>
2021-09-13 23:55:11 -0400

    working on noise

--- src/shaders/noise-frag.glsl
+++ src/shaders/noise-frag.glsl
@@ -32,23 +32,23 @@ vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
 float noise(vec3 p) {
     vec3 a = floor(p);
     vec3 d = p - a;
-    d = d * d * (3.0 - 2.0 * d);
+    d = d * d * (3.f - 2.f * d);
 
-    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 b = a.xxyy + vec4(0.f, 1.f, 0.f, 1.f);
     vec4 k1 = perm(b.xyxy);
     vec4 k2 = perm(k1.xyxy + b.zzww);
 
     vec4 c = k2 + a.zzzz;
     vec4 k3 = perm(c);
-    vec4 k4 = perm(c + 1.0);
+    vec4 k4 = perm(c + 1.f);
 
-    vec4 o1 = fract(k3 * (1.0 / 41.0));
-    vec4 o2 = fract(k4 * (1.0 / 41.0));
+    vec4 o1 = fract(k3 * (1.f / 41.f));
+    vec4 o2 = fract(k4 * (1.f / 41.f));
 
-    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+    vec4 o3 = o2 * d.z + o1 * (1.f - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.f - d.x);
 
-    return o4.y * d.y + o4.x * (1.0 - d.y);
+    return o4.y * d.y + o4.x * (1.f - d.y);
 }
 
 void main()
@@ -72,6 +72,22 @@ void main()
         float time = float(u_Time);
         // vec4 pct = fs_Pos;
         float pct = abs(sin(time));
+        
+            //achieve a interesting color palette by not normalizing light vec
+        float t = dot(normalize(fs_Nor), fs_LightVec);
+        float t2 = dot(normalize(fs_Nor), normalize(fs_LightVec));
 
-        out_Col = mix(out_Col, out_Col+fs_Pos,0.5);
+        vec3 a = vec3(1.8, 0.5, 0.5);
+        vec3 b = vec3(0.5, 0.5, 0.5);
+        vec3 c = vec3(1.f, 0.5, 1.f);
+        vec3 c2 = vec3(1.f, 2, 1.f);
+        vec3 d = vec3(0.f, 1.38, 0.67);
+
+        // vec3 col1 = vec3(noise(fs_Pos));
+        vec3 col2 = vec3(a + b * cos(2.f * 3.14159f * (c2 * t2 + d)));
+
+        out_Col = mix(out_Col, out_Col+fs_Pos,pct/10.f);
+        // out_Col = mix(out_Col, vec3(noise(fs_Pos)*out_Col), vec3(cos(time/10.f)));
+        // out_Col = mix(out_Col, col2, vec3(cos(time/10)));
+
 }
